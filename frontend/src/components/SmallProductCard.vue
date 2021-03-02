29007279b6458a1cf862ebe171755d85e2fba69f
Emma Dawson <emma.l.dawson@gmail.com>
2021-03-02 14:38:45 +0100

    Admin page and small product admin styling

--- frontend/src/components/SmallProductCard.vue
+++ frontend/src/components/SmallProductCard.vue
@@ -10,24 +10,89 @@
     <p class="small article-num">Article No: {{prod._id}}</p>
   </div>
   <div class="prod-edit">
-    <section class="amount">
+    <section class="amount" v-if="$route.path=== '/cart'">
     <div class="dark-bg" @click="removeItemFromCart(prod._id)"><p class="large">-</p></div>
     <div><p class="large">{{prod.amount}}</p></div>
     <div class="dark-bg" @click="addItemToCart(prod._id)"><p class="large">+</p></div>
+    </section>
+    <section v-if="$route.path === '/admin'">
+    <img src="@/assets/icons/edit.svg" alt="delete icon" @click="openEditModal" />
     </section>
     <img src="@/assets/icons/delete.svg" alt="delete icon"
     @click="deleteItemFromCart(prod._id)">
     <p class="large">{{prod.price}} kr</p>
   </div>
+  <BaseModal v-if="toggleModal" @click.native="closeModal">
+  <section class="modal-inner">
+      <section class="delete-info" v-if="deleteOrEdit === 'delete'">
+        <p class="bold">Are you sure you want to delete item from the store?</p>
+        <p>This action <strong>cannot</strong> be undone</p>
+        <BaseButton color="pink" class="btn">CONFIRM</BaseButton>
+        <BaseButton color="offwhite" class="btn" @click.native="closeModal">CANCEL</BaseButton>
+      </section>
+      <section class="edit-info" v-if="deleteOrEdit === 'edit'">
+        <form class="information-wrapper">
+      <p class="bold">Edit item</p>
+
+      <div class="view-row">
+        <label>Title</label>
+        <input type="text" v-model="product.title" />
+      </div>
+
+      <div class="view-row">
+        <label>Price:</label>
+        <input type="text" v-model="product.price"/>
+      </div>
+
+      <div class="view-row">
+        <label>Short Desc:</label>
+        <input type="text" v-model="product.shortDesc" />
+      </div>
+
+      <div class="view-row">
+        <label>Long desc:</label>
+        <input type="text" v-model="product.longDesc" />
+      </div>
+
+      <div class="view-row">
+        <label>Image file:</label>
+        <input type="text" v-model="product.imgFile" />
+      </div>
+      <div class="edit-buttons">
+       <BaseButton color="offwhite" class="btn" @click.native="closeModal">CANCEL</BaseButton>
+      <BaseButton color="teal" class="btn">Edit Product</BaseButton>
+    </div>
+  </form>
+      </section>
+  </section>
+  </BaseModal>
 
   
 </article>
 </template>
 
 <script>
+import BaseModal from "@/components/BaseModal.vue"
 export default {
 props: {
   prod: Object  },
+  components: {
+    BaseModal
+  },
+  data() {
+    return {
+      toggleModal: false,
+      deleteOrEdit: "",
+      product: {
+        _id: this.prod._id,
+        title: this.prod.title,
+        price: this.prod.price,
+        shortDesc: this.prod.shortDesc,
+        longDesc: this.prod.longDesc,
+        imgFile: this.prod.imgFile
+      }
+    }
+  },
   computed: {
     imageFile() {
     return require(`@/assets/products/${this.prod.imgFile}`)
@@ -41,7 +106,21 @@ props: {
       this.$store.dispatch("removeFromCart", id)
     },
     deleteItemFromCart(id) {
-      this.$store.dispatch("deleteFromCart", id)
+      if(this.$route.path === "/cart") {
+        this.$store.dispatch("deleteFromCart", id)
+      }
+      if (this.$route.path === "/admin") {
+        this.toggleModal = true;
+        this.deleteOrEdit = "delete"
+      }
+      
+    },
+    openEditModal() {
+      this.toggleModal = true;
+      this.deleteOrEdit = "edit"
+    },
+    closeModal() {
+      this.toggleModal = false
     }
   }
 }
@@ -117,6 +196,80 @@ article {
       cursor: pointer;
     }
   }
+}
+
+.modal-inner {
+  background-color: #fff;
+  width: 45rem;
+  
+  border-radius: 8px;
+  padding: 4rem 2rem;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .bold {
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 2rem;
+
+  }
+
+  .btn {
+    margin-top: 3rem;
+  }
+
+  .btn + .btn {
+    margin-left: 2rem;
+  }
+
+  .information-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    margin: auto;
+    
+    .edit-buttons {
+      align-self: flex-end;
+    }
+
+    .bold {
+      font-size: 2rem;
+      font-weight: 700;
+      
+    }
+
+    
+
+    .view-row {
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      margin-top: 2rem;
+      width: 100%;
+
+      label {
+        font-size: 1.2rem;
+        min-width: 10rem;
+      }
+
+      .info,
+      input {
+        background-color: $off-white;
+        border: 1px solid $secondary-clr-dk;
+        font-family: inherit;
+        font-size: 1.4rem;
+        height: 3rem;
+        outline: none;
+        padding: 0.5rem;
+        width: 75%;
+      }
+
+      
+    }
+  }
+
 }
 
 </style>
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,13 +1,160 @@
 <template>
-<section>Admin</section>
+<section 
+v-if="!userRole || userRole.role === 'customer'"
+class="no-access"
+>
+<h2>ACCESS DENIED</h2>
+<p>You do not have access to this page.</p>
+<p>Please contact your site administrator if this is a mistake.</p>
+</section>
+
+<section v-else class="admin-wrapper">
+  <h2>ADMIN</h2>
+  
+  <div class="flex-wrapper">
+  <form class="information-wrapper">
+      <p class="bold">Add new item</p>
+
+      <div class="view-row">
+        <label>Title</label>
+        <input type="text"  />
+      </div>
+
+      <div class="view-row">
+        <label>Price:</label>
+        <input type="text" />
+      </div>
+
+      <div class="view-row">
+        <label>Short Desc:</label>
+        <input type="text"  />
+      </div>
+
+      <div class="view-row">
+        <label>Long desc:</label>
+        <input type="text"  />
+      </div>
+
+      <div class="view-row">
+        <label>Image file:</label>
+        <input type="text"  />
+      </div>
+
+      <BaseButton color="teal" class="btn">Add Product</BaseButton>
+  </form>
+
+  <section class="products">
+    <h2>ALL PRODUCTS</h2>
+    <SmallProductCard 
+    v-for="product in products" 
+    :key="product._id" 
+    :prod="product"
+    class="card" />
+  </section>
+  </div>
+
+
+</section>
 </template>
 
 <script>
+import SmallProductCard from "@/components/SmallProductCard.vue"
 export default {
-
+components: {
+  SmallProductCard
+},
+computed: {
+  products() {
+    return this.$store.getters.getProducts
+  },
+  userRole() {
+    console.log(this.$store.getters.getCurrentUser);
+    return this.$store.getters.getCurrentUser
+  }
+}
 }
 </script>
 
 <style lang="scss" scoped>
 @import "@/styles/_variables.scss";
+
+.no-access {
+  height: 70rem;
+  padding: 5rem;
+  text-align: center;
+
+  h2 {
+    margin-bottom: 5rem;
+  }
+
+  p {
+    font-size: 2rem;
+    line-height: 1.6;
+  }
+}
+
+.admin-wrapper {
+  max-width: 100rem;
+  margin: auto;
+
+  h2 {
+  text-align: center;
+  margin: 5rem 0;
+}
+
+  .information-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 70%;
+    margin: auto;
+    
+
+    .bold {
+      font-size: 2rem;
+      font-weight: 700;
+      
+    }
+
+    .btn {
+      width: 30%;
+      margin: 3rem 0 3rem auto;
+      
+    }
+
+    .view-row {
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      margin-top: 2rem;
+      width: 100%;
+
+      label {
+        font-size: 1.2rem;
+        min-width: 10rem;
+      }
+
+      .info,
+      input {
+        background-color: $off-white;
+        border: 1px solid $secondary-clr-dk;
+        font-family: inherit;
+        font-size: 1.4rem;
+        height: 3rem;
+        outline: none;
+        padding: 0.5rem;
+        width: 75%;
+      }
+    }
+  }
+
+  .products {
+    max-width: 50%;
+    margin: 3rem auto;
+
+    .card {
+      margin-top: 2rem;
+    }
+    
+  }
+}
 </style>
